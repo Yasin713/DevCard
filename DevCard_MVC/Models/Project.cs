b41d26e46f5eab2f65ee17945cc7b977dfb499d0
Yasin713 <50019427+Yasin713@users.noreply.github.com>
2022-10-16 21:05:42 +0330

    projects section moved from  index to partial

---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+
+    }
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Description { get; set; }
+
+    }
+    public class IndexModel
+    {
+        public List<Project> Projects { get; set; }
+        public List<Article> Articles { get; set; }
+
+    }
+}
